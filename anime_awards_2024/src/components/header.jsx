c8a10701e6cfafca0498ff12fc8b66b0d83339ca
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-06-02 22:43:04 -0300

    1.1

--- anime_awards_2024/src/components/header.jsx
+++ anime_awards_2024/src/components/header.jsx
@@ -1,10 +1,13 @@
 import styles from "./modulos_css/header.module.css"
+import nekologo from "../assets/logo.png"
+import idioma from "../assets/icones/idioma_icone.png"
+import conta from "../assets/icones/conta_icone.png"
 
 function Header() {
     return (
         <div>
             <header className={styles.header}>
-                <img src="" alt="Imagem-logo" />
+                <img src={nekologo} alt="Imagem-logo" className={styles.logo_img} />
                 <div className={styles.navs}>
                     <nav>Filmes</nav>
                     <nav>Populares</nav>
@@ -14,9 +17,9 @@ function Header() {
                     <nav>Categorias</nav>
                 </div>
                 <input type="search" placeholder="Pesquisar Anime"/>
-                <div>
-                    <img src="" alt="" className={styles.icones}/>
-                    <img src="" alt="" className={styles.icones}/>
+                <div className={styles.conta_idioma}>
+                    <img src={conta} alt="" className={styles.icones}/>
+                    <img src={idioma} alt="" className={styles.icones}/>
                 </div>
             </header>
         </div>
