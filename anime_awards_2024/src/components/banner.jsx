29367d7a43047c5f6eb4460cb5ac2d44684c595d
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-06-04 21:09:14 -0300

    1.3

--- anime_awards_2024/src/components/banner.jsx
+++ anime_awards_2024/src/components/banner.jsx
@@ -1,19 +1,115 @@
 import styles from "./modulos_css/banner.module.css"
 import bannerNaruto from "../assets/fotos-banner/naruto.jpg"
-import logoNatuto from "../assets/fotos-banner/logo-naruto.png"
+import bannerJujutsu from "../assets/fotos-banner/jujutsu.jpg"
+import bannerJojo from "../assets/fotos-banner/jojo.jpg"
+import bannerDbz from "../assets/fotos-banner/dbz.jpg"
+
+import logoNaruto from "../assets/fotos-banner/logo-naruto.png"
+import logoJujutsu from "../assets/fotos-banner/logo-jujutsu.png"
+import logoJojo from "../assets/fotos-banner/logo-jojos.png"
+import logoDbz from "../assets/fotos-banner/logo-dbz.png"
+import { useEffect, useState } from "react"
+
+const bannerData = [
+    {
+        id: 0,
+        logoImg: logoNaruto,
+        classLogo: styles.naruto,
+        age: "+12",
+        genres: ["SHONEN", "AVENTURA"],
+        sinopse:
+        "Naruto acompanha a história de Naruto Uzumaki, um jovem ninja que sonha em se tornar o maior líder de sua vila, o Hokage. Marcado desde o nascimento por abrigar dentro de si a poderosa Raposa de Nove Caudas, ele enfrenta o preconceito dos moradores enquanto busca reconhecimento, amizade e força para proteger quem ama.",
+    },
+    {
+        id: 1,
+        logoImg: logoJujutsu,
+        classLogo: styles.jujutsu_logo,
+        age: "+16",
+        genres: ["SHONEN", "AÇÃO", "FANTASIA"],
+        sinopse:
+        "Yuji Itadori, um estudante do ensino médio, se envolve com o mundo das maldições e se torna hospedeiro de Sukuna, um poderoso espírito amaldiçoado. Ele se junta à Escola Jujutsu para aprender a controlar seus poderes e lutar contra outras maldições.",
+    },
+    {
+        id: 2,
+        logoImg: logoJojo,
+        classLogo: styles.jojos_logo,
+        age: "+14",
+        genres: ["AVENTURA", "SOBRENATURAL"],
+        sinopse:
+        "JoJo's Bizarre Adventure segue a linhagem da família Joestar, que possui habilidades sobrenaturais. Cada parte da série foca em um membro diferente da família e suas aventuras contra forças do mal.",
+    },
+    {
+        id: 3,
+        logoImg: logoDbz,
+        classLogo: styles.dbz_logo,
+        age: "+10",
+        genres: ["AÇÃO", "LUTAS", "SCI-FI"],
+        sinopse:
+        "Dragon Ball Z narra as aventuras de Goku e seus amigos, que defendem a Terra contra uma variedade de vilões intergalácticos. A série é conhecida por suas intensas batalhas e transformações.",
+    },
+];
 
 function Banner() {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const totalSlides = 4;
+
+    useEffect (() => {
+        let timeout;
+
+        function autoSlide() {
+            setCurrentSlide(prev => (prev + 1) % totalSlides);
+            timeout = setTimeout(autoSlide, 5000);
+        }
+
+        timeout = setTimeout(autoSlide, 5000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    function handleRadioClick(index) {
+        setCurrentSlide(index);
+    }
+
+    const currentBannerInfo = bannerData[currentSlide];
+
     return(
-        <div className={styles.box_img}><img src={bannerNaruto} alt="Naruto-banner" />
-            <div className={styles.container_logos}>
-                <img src={logoNatuto} alt="naruto-logo" />
+        <div className={styles.slider_container} 
+        style={{marginLeft: `-${currentSlide * 100}vw`, transition: `margin-left 0.5s ease-in-out`}}>
+            
+            <div className={styles.box_img}>
+                
+                <img src={bannerNaruto} alt="Naruto-banner" />
+            </div>
+
+            <div className={styles.box_img}>
+
+                <img src={bannerJujutsu} alt="jujutsu-banner" />
+
+            </div>
+
+            <div className={styles.box_img}>
+
+                <img src={bannerJojo} alt="jojo-banner" />
+
+            </div>
+
+            <div className={styles.box_img}>
+
+                <img src={bannerDbz} alt="db-banner" />
+
+            </div>
+
+            <div className={`${styles.container_logos} ${currentBannerInfo}`}>
+                <img src={currentBannerInfo.logoImg} alt="logo-anime"/>
                 <div className={styles.info_banner}>
-                    <p>+12</p>
-                    <p>SHONEN</p>
-                    <p>AVENTURA</p>
+                    <p>{currentBannerInfo.age}</p>
+                    {currentBannerInfo.genres.map((genre, i) => (
+                        <p key={i}>{genre}</p>
+                    ))}
                 </div>
                 <div className={styles.sinopse}>
-                    <p>Naruto acompanha a história de Naruto Uzumaki, um jovem ninja que sonha em se tornar o maior líder de sua vila, o Hokage. Marcado desde o nascimento por abrigar dentro de si a poderosa Raposa de Nove Caudas, ele enfrenta o preconceito dos moradores enquanto busca reconhecimento, amizade e força para proteger quem ama.</p>
+                    <p>{currentBannerInfo.sinopse}</p>
                 </div>
                 <div className={styles.container_buttons}>
                     <button className={styles.assistir_btn}>Assistir</button>
@@ -22,10 +118,14 @@ function Banner() {
             </div>
 
             <div className={styles.animes_buttons}>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <input type="radio" name="banner-radio" value={0} checked={currentSlide === 0} onChange={() => handleRadioClick(0)}/>
+
+                <input type="radio" name="banner-radio" value={1} checked={currentSlide === 1} onChange={() => handleRadioClick(1)}/>
+
+                <input type="radio" name="banner-radio" value={2} checked={currentSlide === 2} onChange={() => handleRadioClick(2)}/>
+
+                <input type="radio" name="banner-radio" value={3} checked={currentSlide === 3} onChange={() => handleRadioClick(3)}/>
+                
             </div>
         </div>
     )
