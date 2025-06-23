a00eaf0a222d42e39b6719a30e3fdcfcffb3cb3c
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-06-23 18:00:25 -0300

    2.7

--- anime_awards_2024/src/components/header.jsx
+++ anime_awards_2024/src/components/header.jsx
@@ -2,14 +2,41 @@ import styles from "./modulos_css/header.module.css"
 import nekologo from "../assets/logo.png"
 import idioma from "../assets/icones/idioma_icone.png"
 import conta from "../assets/icones/conta_icone.png"
+import menu from "../assets/icones/mobile_menu.png"
+import { useState } from "react"
 
 function Header() {
+    
+    const [estaAtivo, setEstaAtivo] = useState(false);
+
+    const handleClick = () => {
+        setEstaAtivo(prev => !prev);
+    }
     return (
         <div>
             <header className={styles.header}>
                 <img src={nekologo} alt="Imagem-logo" className={styles.logo_img} />
                 <div className={styles.navs}>
-                    <nav>
+                    <nav className={`${styles.mobile_menu} ${estaAtivo ? styles.ativo : ""}`}>
+                        <ul>
+                            <li><a href="#">Conta</a></li>
+                            <li><a href="#">Populares</a></li>
+                            <li><a href="#">Lançamentos</a></li>
+                            <li><a href="#">Minha lista</a></li>
+                            <li><a href="#">Favoritos</a></li>
+                            <li>
+                                <a href="#">Categorias <span className={styles.setinha_emogi}>▼</span></a>
+                                <ul className={styles.dropdown}>
+                                    <li>Shounen</li>
+                                    <li>Seinen</li>
+                                    <li>Isekai</li>
+                                    <li>Romance</li>
+                                    <li>Comédia</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                    <nav className={styles.desktop_menu}>
                         <ul>
                             <li><a href="#">Populares</a></li>
                             <li><a href="#">Lançamentos</a></li>
@@ -30,8 +57,9 @@ function Header() {
                 </div>
                 <input type="search" placeholder="Pesquisar Anime"/>
                 <div className={styles.conta_idioma}>
-                    <img src={conta} alt="" className={styles.icones} />
-                    <img src={idioma} alt="" className={styles.icones}/>
+                    <img src={conta} alt="criar conta imagem" className={`${styles.icones} ${styles.conta}`} />
+                    
+                    <img src={menu} alt="mobile menu imagem" className={`${styles.icones} ${styles.menu}`} onClick={handleClick}/>
                 </div>
             </header>
         </div>
